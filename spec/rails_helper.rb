a22a430cdb42cdda756338c020eb5c6dd0377089
Marcos Lutterbach <mlutterb@gmail.com>
2024-10-16 19:42:43 +0200

    Added gem simplecov for rspec test coverage

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
+require 'simplecov'
+SimpleCov.start
 require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
@@ -32,7 +34,7 @@ RSpec.configure do |config|
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
   end
-  
+
   config.before(:each) do
     DatabaseCleaner.start # Start cleaning before each test
   end
